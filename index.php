857922239f66f3a8f3aee79bd975239fa597f068
sitesjt <111943790+sitesjt@users.noreply.github.com>
2022-08-24 13:11:41 -0300

    sadadsadadas

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>BetaMuOnline</title>
+</head>
+<body bgcolor="black">dssdsdds
+<iframe src=”http://betamuonline.sytes.net:8080/” width="1300" height="2000" > sddad\sadadadddddddddddddddddddddddddddddddddd
+</iframe>
+</body>
+</html>
